2ea70a42e75292e06f3776cba1260d1fd34bdc50
teamlead11 <automationlead11@gmail.com>
2018-08-30 09:34:09 +0530

    added logic to add three numbers

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -6,6 +6,10 @@ public class BaseClass {
 		// TODO Auto-generated method stub
 		System.out.println("this is my base class");
 		System.out.println("code added by satish");
+		add(6, 7, 8);
 	}
 
+	public static void add(int a, int b, int c) {
+		System.out.println(a + b + c);
+	}
 }
