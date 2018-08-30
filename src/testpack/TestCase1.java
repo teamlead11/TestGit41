0d7ccd65c83cf5a7c814f7840c951682cc2a7a98
vicz95 <mail2vignesh95@gmail.com>
2018-08-30 09:30:07 +0530

    added a line in testcase1

--- src/testpack/TestCase1.java
+++ src/testpack/TestCase1.java
@@ -5,6 +5,7 @@ public class TestCase1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is my test case one");
+		System.out.println("this line is added");
 	}
 
 }
